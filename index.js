9654605da6c4a11db203798fce36e62e87145ada
antrikshverma <priyanshu.creator.dev@gmail.com>
2023-05-03 17:46:34 +0530

    dev removed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 require("./db");
 const getRoutes = require("./routers/get.router");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // express injections with middlewares
-app.use(morgan("common"));
+// app.use(morgan("common"));
 app.use(cors());
 app.use(express.json());
 app.use("/api", getRoutes);
